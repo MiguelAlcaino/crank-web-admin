0ef19dc56e8deb85cf3771a632a756427cc42b6c
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2024-03-29 10:59:32 -0300

    Create StatIconWidget.vue

---
+++ src/components/StatIconWidget.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  icon: 'total_energy' | 'calories' | 'distance' | 'duration'
+  value: string
+}>()
+</script>
+
+<template>
+  <div class="row p-3">
+    <div class="col-12 text-center">
+      <h4>{{ title }}</h4>
+    </div>
+    <div class="col-6 text-right">
+      <i v-if="icon === 'duration'" class="bi bi-stopwatch-fill" style="font-size: 4rem"></i>
+      <i v-if="icon === 'distance'" class="bi bi-arrows-angle-expand" style="font-size: 4rem"></i>
+      <i v-if="icon === 'calories'" class="bi bi-fire" style="font-size: 4rem"></i>
+      <i v-if="icon === 'total_energy'" class="bi bi-star-fill" style="font-size: 4rem"></i>
+    </div>
+    <div class="col-6 text-left align-middle">
+      <p>{{ value }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped src="bootstrap/dist/css/bootstrap.min.css"></style>
+<style lang="css" scoped src="@/assets/main.css"></style>
+
+<style scoped>
+p {
+  font-family: 'BigJohn', sans-serif;
+  font-size: x-large;
+}
+h4 {
+  color: #737373;
+}
+</style>
