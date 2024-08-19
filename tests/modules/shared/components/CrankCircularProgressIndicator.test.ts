7c129777c845229b33aaa9db75c2c8f36399236b
Gabriel Correa Ramírez <26012694+Gaboae86@users.noreply.github.com>
2024-08-19 19:11:00 -0400

    chore: Update CrankCircularProgressIndicator test file and snapshot

---
+++ tests/modules/shared/components/CrankCircularProgressIndicator.test.ts
@@ -0,0 +1,40 @@
+import { mount } from '@vue/test-utils'
+import CrankCircularProgressIndicator from '@/modules/shared/components/CrankCircularProgressIndicator.vue'
+
+describe('<CrankCircularProgressIndicator />', () => {
+  test('should match snapshot', () => {
+    const wrapper = mount(CrankCircularProgressIndicator, {
+      props: {}
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('should render value text correctly', () => {
+    const text = 'Please wait'
+
+    const wrapper = mount(CrankCircularProgressIndicator, {
+      props: {
+        text: text
+      }
+    })
+
+    expect(wrapper.find('p strong').text()).toBe(text)
+  })
+
+  test('renders without text prop', () => {
+    const wrapper = mount(CrankCircularProgressIndicator)
+    expect(wrapper.find('.crankSpiner').exists()).toBe(true)
+    expect(wrapper.find('p').exists()).toBe(false)
+  })
+
+  test('renders with text prop', () => {
+    const text = 'Please wait'
+    const wrapper = mount(CrankCircularProgressIndicator, {
+      props: { text }
+    })
+    expect(wrapper.find('.crankSpiner').exists()).toBe(true)
+    expect(wrapper.find('p').exists()).toBe(true)
+    expect(wrapper.find('p strong').text()).toBe(text)
+  })
+})
