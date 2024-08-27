459177320483d918c7f1b3aa7784726d4e2536a0
Gabriel Correa Ramírez <26012694+Gaboae86@users.noreply.github.com>
2024-08-26 21:59:27 -0400

    Update Vite configuration: define process.env

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,39 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { Config } from './src/model/Config'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
-      name: "crank-web-admin",
-      fileName: 'crank-web-admin'
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return defineConfig({
+    define: {
+      'process.env': env
     },
-    rollupOptions: {
-
-    }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: Config.AUTH_SERVICE_HOST,
-        changeOrigin: true,
-        secure: false,
-        ws: true
+    plugins: [vue()],
+    build: {
+      lib: {
+        entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+        name: 'crank-web-admin',
+        fileName: 'crank-web-admin'
+      },
+      rollupOptions: {}
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: Config.AUTH_SERVICE_HOST,
+          changeOrigin: true,
+          secure: false,
+          ws: true
+        }
       }
     }
-  }
-})
+  })
+}
