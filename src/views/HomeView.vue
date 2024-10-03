03c66c66a68e939a2dcceefad6548d9e5fa29c7f
Gabriel Correa Ramírez <26012694+Gaboae86@users.noreply.github.com>
2024-10-03 11:48:13 -0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,12 +8,13 @@
     <RouterLink key="/admin/calendar-class" to="/admin/calendar-class"> Admin Class</RouterLink>
     <br />
     <RouterLink key="/admin/calendar-class" to="/admin/room-layout/list">
-      Admin Room Layout</RouterLink
-    >
+      Admin Room Layout</RouterLink>
     <br />
     <RouterLink key="/user/create" to="/user/create"> Create User</RouterLink>
     <br />
     <RouterLink key="/class-schedules" to="/class-schedules"> Class Schedules</RouterLink>
+    <br />
+    <RouterLink key="/admin-users" to="/admin-users"> Admin Users</RouterLink>
     <br />
   </main>
 </template>
