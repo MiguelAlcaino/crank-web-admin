bb2ebdd5bd6ce90016824e7ee92b9d5c51326265
Gabriel Correa Ramírez <26012694+Gaboae86@users.noreply.github.com>
2024-10-04 19:14:56 -0300

    Create useAdminUserList.ts

---
+++ src/modules/admin-user/composables/useAdminUserList.ts
@@ -0,0 +1,36 @@
+import { ApiService } from '@/services/apiService'
+import { onMounted, readonly, ref } from 'vue'
+import { AdminUser } from '../interfaces'
+
+const adminUsers = ref<AdminUser[]>([])
+
+export const useAdminUserList = (apiService: ApiService) => {
+  const hasError = ref<boolean>(false)
+  const isLoading = ref<boolean>(false)
+
+  onMounted(() => {
+    getAdminUsers()
+  })
+
+  async function getAdminUsers() {
+    hasError.value = false
+    isLoading.value = true
+
+    adminUsers.value = []
+
+    try {
+      adminUsers.value = (await apiService.getAdminUsers()) as AdminUser[]
+    } catch (error) {
+      hasError.value = true
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  return {
+    // Properties
+    isLoading: readonly(isLoading),
+    adminUsers: adminUsers,
+    hasError: hasError
+  }
+}
