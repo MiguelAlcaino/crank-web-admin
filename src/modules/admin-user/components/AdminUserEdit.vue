0270d99d1ed8683880e901b94afd003647752edb
Gabriel Correa Ramírez <26012694+Gaboae86@users.noreply.github.com>
2024-10-04 20:15:19 -0300

    Update AdminUserEdit.vue

--- src/modules/admin-user/components/AdminUserEdit.vue
+++ src/modules/admin-user/components/AdminUserEdit.vue
@@ -1,7 +1,217 @@
+<script setup lang="ts">
+import { computed, inject, reactive, ref } from 'vue'
+
+import type { ApiService } from '@/services/apiService'
+
+import Multiselect from 'vue-multiselect'
+import 'vue-multiselect/dist/vue-multiselect.min.css'
+import type { AdminUser, Instructor } from '../interfaces'
+import { Site } from '@/modules/shared/interfaces/site'
+import { Role } from '@/utils/userRoles'
+import { email, helpers, required } from '@vuelidate/validators'
+import useVuelidate from '@vuelidate/core'
+import { SiteEnum } from '@/gql/graphql'
+import { ERROR_UNKNOWN } from '@/utils/errorMessages'
+
+import DefaultButtonComponent from '@/modules/shared/components/DefaultButtonComponent.vue'
+import ModalComponent from '@/modules/shared/components/ModalComponent.vue'
+
+const apiService = inject<ApiService>('gqlApiService')!
+
+const props = defineProps<{
+  adminUser: AdminUser
+}>()
+
+const modalIsVisible = ref<boolean>(false)
+const isSaving = ref<boolean>(false)
+
+const instructors = ref<Instructor[]>([])
+const loadingInstructors = ref<boolean>(false)
+
+const availableSites = ref<Site[]>([])
+const loadingSites = ref<boolean>(false)
+
+const instructorsControlIsVisible = ref<boolean>(false)
+
+const successModalIsVisible = ref<boolean>(false)
+const errorMessage = ref<string>('')
+const errorModalIsVisible = ref<boolean>(false)
+
+const roleOptions = [
+  { label: 'Super Admin', value: Role.ROLE_SUPER_ADMIN },
+  { label: 'Staff', value: Role.ROLE_STAFF },
+  { label: 'Instructor', value: Role.ROLE_INSTRUCTOR }
+]
+
+const formData = reactive({
+  username: '',
+  email: '',
+  role: null as string | null,
+  linkedInstructorIds: [] as string[],
+  linkedSiteCodes: [] as SiteEnum[]
+})
+
+const selectedInstructors = computed({
+  get() {
+    return formData.linkedInstructorIds
+      .map((id) => instructors.value.find((instructor) => instructor.id === id))
+      .filter((instructor): instructor is Instructor => instructor !== undefined)
+  },
+  set(selected) {
+    formData.linkedInstructorIds = selected.map((instructor) => instructor.id)
+  }
+})
+
+const selectedSites = computed({
+  get() {
+    return formData.linkedSiteCodes
+      .map((code) => availableSites.value.find((site) => site.code === code))
+      .filter((site): site is Site => site !== undefined)
+  },
+  set(selected) {
+    formData.linkedSiteCodes = selected.map((site) => site.code)
+  }
+})
+
+const rules = computed(() => {
+  return {
+    username: {
+      required: helpers.withMessage('Username is required', required)
+    },
+    email: {
+      required: helpers.withMessage('Email is required', required),
+      email: helpers.withMessage('The email address is not valid', email)
+    },
+    role: {
+      required: helpers.withMessage('Rol is required', required)
+    },
+    linkedInstructorIds: {},
+    linkedSiteCodes: {}
+  }
+})
+
+const v$ = useVuelidate(rules, formData, { $scope: false })
+
+const openModal = () => {
+  v$.value.$reset()
+
+  instructorsControlIsVisible.value = false
+
+  selectedInstructors.value = []
+  selectedSites.value = []
+
+  formData.email = props.adminUser.email
+  formData.username = props.adminUser.username
+  formData.role = null
+  formData.linkedInstructorIds = props.adminUser.linkedInstructors
+    ? props.adminUser.linkedInstructors.map((i) => i.id)
+    : []
+  formData.linkedSiteCodes = props.adminUser.linkedSites
+    ? props.adminUser.linkedSites.map((s) => s.code)
+    : []
+
+  let role = null as string | null
+
+  if (props.adminUser.roles) {
+    if (props.adminUser.roles.includes(Role.ROLE_SUPER_ADMIN)) {
+      role = Role.ROLE_SUPER_ADMIN
+    } else if (props.adminUser.roles.includes(Role.ROLE_STAFF)) {
+      role = Role.ROLE_STAFF
+    } else if (props.adminUser.roles.includes(Role.ROLE_INSTRUCTOR)) {
+      role = Role.ROLE_INSTRUCTOR
+      instructorsControlIsVisible.value = true
+    }
+  }
+
+  formData.role = role
+
+  getAvailableSites()
+  getAvailableInstructors()
+
+  modalIsVisible.value = true
+}
+
+const closeModal = () => {
+  modalIsVisible.value = false
+}
+
+const submitForm = async () => {
+  const isValid = await v$.value.$validate()
+
+  console.log('formData', formData)
+
+  if (isValid) {
+    try {
+      isSaving.value = true
+
+      var response = await apiService.updateAdminUser({
+        adminUserId: props.adminUser.id,
+        userDataInput: {
+          username: formData.username,
+          email: formData.email,
+          role: formData.role!,
+          linkedInstructorIds: formData.linkedInstructorIds,
+          linkedSiteCodes: formData.linkedSiteCodes
+        }
+      })
+
+      if (response.__typename == 'AdminUser') {
+        closeModal()
+        successModalIsVisible.value = true
+      } else if (response.__typename == 'EmailAlreadyUsedError') {
+        errorMessage.value =
+          'The provided email is already used by another user. Please choose another one.'
+        errorModalIsVisible.value = true
+      } else if (response.__typename == 'UsernameAlreadyUsedError') {
+        errorMessage.value =
+          'The provided username is already used by another user. Please choose another one.'
+        errorModalIsVisible.value = true
+      } else {
+        errorModalIsVisible.value = true
+        errorMessage.value = ERROR_UNKNOWN
+      }
+    } catch (error) {
+      errorModalIsVisible.value = true
+      errorMessage.value = ERROR_UNKNOWN
+    } finally {
+      isSaving.value = false
+    }
+  }
+}
+
+const onChangeRole = () => {
+  formData.linkedInstructorIds = []
+  instructorsControlIsVisible.value = formData.role === Role.ROLE_INSTRUCTOR
+}
+
+async function getAvailableInstructors(): Promise<void> {
+  instructors.value = []
+  loadingInstructors.value = true
+
+  try {
+    instructors.value = await apiService.getAvailableInstructors()
+  } catch (error) {
+    // ignore
+  } finally {
+    loadingInstructors.value = false
+  }
+}
+
+async function getAvailableSites(): Promise<void> {
+  availableSites.value = []
+  loadingSites.value = true
+  try {
+    availableSites.value = await apiService.getAvailableSites()
+  } catch (error) {
+    // ignore
+  } finally {
+    loadingSites.value = false
+  }
+}
+</script>
+
 <template>
-  <div>
-    <DefaultButtonComponent text="Edit" type="button" @on-click="onCreateANewUser" />
-  </div>
+  <DefaultButtonComponent text="Edit" type="button" @on-click="openModal" />
 
   <transition name="modal" v-if="modalIsVisible">
     <div class="modal-mask">
@@ -9,7 +219,7 @@
         <div class="modal-dialog modal-lg" role="document">
           <div class="modal-content">
             <div class="modal-header border-0">
-              <h5 class="modal-title">Edit user {{ selectedAdminUser?.username }}</h5>
+              <h5 class="modal-title">Edit user {{ adminUser?.username }}</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true" @click="closeModal">&times;</span>
               </button>
@@ -74,7 +284,7 @@
                       <select
                         class="custom-select"
                         required
-                        v-model="formData.rol"
+                        v-model="formData.role"
                         placeholder="Rol"
                         @change="onChangeRole"
                       >
@@ -89,7 +299,7 @@
                     </div>
 
                     <small
-                      v-for="error in v$.rol.$errors"
+                      v-for="error in v$.role.$errors"
                       :key="error.$uid"
                       class="form-text"
                       style="color: red"
@@ -114,7 +324,7 @@
                         placeholder="Mindbody staff"
                         label="name"
                         track-by="id"
-                        :preselect-first="true"
+                        :preselect-first="false"
                         class="custom-multiselect"
                         :loading="loadingInstructors"
                       >
@@ -182,6 +392,7 @@
                 text="Update"
                 type="button"
                 :is-loading="isSaving"
+                :disabled="loadingSites || loadingInstructors"
                 @on-click="submitForm"
               >
               </DefaultButtonComponent>
@@ -191,46 +402,30 @@
       </div>
     </div>
   </transition>
+
+  <!-- Error Modal -->
+  <ModalComponent
+    v-if="errorModalIsVisible"
+    title="ERROR"
+    :message="errorMessage"
+    :closable="false"
+    @on-ok="errorModalIsVisible = false"
+    :cancel-text="null"
+  >
+  </ModalComponent>
+
+  <!-- Success Modal -->
+  <ModalComponent
+    v-if="successModalIsVisible"
+    title="SUCCESS"
+    message="user successfully updated."
+    :closable="false"
+    @on-ok="successModalIsVisible = false"
+    :cancel-text="null"
+  >
+  </ModalComponent>
 </template>
 
-<script setup lang="ts">
-import DefaultButtonComponent from '@/modules/shared/components/DefaultButtonComponent.vue'
-import type { ApiService } from '@/services/apiService'
-import { inject } from 'vue'
-import { useAdminUser } from '../composables/useAdminUser'
-
-import Multiselect from 'vue-multiselect'
-import 'vue-multiselect/dist/vue-multiselect.min.css'
-import type { AdminUser } from '../interfaces'
-
-const {
-  openModal,
-  modalIsVisible,
-  closeModal,
-  v$,
-  isSaving,
-  submitForm,
-  formData,
-  roleOptions,
-  availableSites,
-  instructors,
-  loadingInstructors,
-  loadingSites,
-  selectedAdminUser,
-  selectedInstructors,
-  selectedSites,
-  onChangeRole,
-  instructorsControlIsVisible
-} = useAdminUser(inject<ApiService>('gqlApiService')!)
-
-const props = defineProps<{
-  adminUser: AdminUser
-}>()
-
-function onCreateANewUser() {
-  openModal(props.adminUser)
-}
-</script>
 <style scoped></style>
 
 <style>
