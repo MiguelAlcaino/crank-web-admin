baac1e718ff224ce8e848dcf654aef1d909a66c4
Gabriel Correa Ramírez <26012694+Gaboae86@users.noreply.github.com>
2024-12-20 16:26:51 -0300

    npm run build

---
+++ dist/CustomerProfileView.6a8a85ec.js
@@ -0,0 +1,6 @@
+import "./index.50180c51.js";
+import { G as f, A as o } from "./index.50180c51.js";
+export {
+  f as GenderEnum,
+  o as default
+};
--- dist/crank-web-admin.js
+++ dist/crank-web-admin.js
@@ -1,4 +1,4 @@
-import { H as s, J as r, C as p, I as e, K as o, L as A, D as C, E as m, M as d } from "./index.f5b08964.js";
+import { H as s, J as r, C as p, I as e, K as o, L as A, D as C, E as m, M as d } from "./index.50180c51.js";
 export {
   s as startCalendarApp,
   r as startClassScheduleApp,
